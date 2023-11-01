a6aad1f1814c8d3509a58ee63a5f91f8e3f9ce3b
alamin <alaminhosen44000@gmail.com>
2023-11-01 11:01:03 +0600

    b

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
+// for mobile offcanvas menu function
+{
+  let offcanvasOpener = document.querySelector(".offmenu-open");
+  let offcanvasCloser = document.querySelector(".offcanvas-close");
+  let offcanvasMenu = document.querySelector(".offcanvas-menu");
+  let offcanvasOverlay = document.querySelector(".offcanvas-overlay");
+  let offcanvasHider = document.querySelectorAll(".off-close");
 
+  // actions
+  offcanvasOpener.addEventListener("click", function () {
+    offcanvasOpen();
+  });
+  offcanvasCloser.addEventListener("click", function () {
+    offcanvasClose();
+  });
+  window.addEventListener("click", function (e) {
+    let container = document.querySelector(".offmenu");
+    if (!container.contains(e.target) && !offcanvasOpener.contains(e.target)) {
+      console.log("outside");
+      offcanvasClose();
+    }
+  });
+  offcanvasHider.forEach((elm) => {
+    elm.addEventListener("click", function () {
+      offcanvasClose();
+    });
+  });
 
-
-
-
-
-
-
-
-
+  // offcanvasMenu Open function
+  function offcanvasOpen() {
+    offcanvasMenu.classList.add("active");
+    offcanvasOverlay.classList.add("active");
+    document.querySelector("body").classList.add("offcanvas-opened");
+  }
+  // offcanvasMenu Close function
+  function offcanvasClose() {
+    offcanvasMenu.classList.remove("active");
+    offcanvasOverlay.classList.remove("active");
+    document.querySelector("body").classList.remove("offcanvas-opened");
+  }
+}
