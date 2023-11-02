fcef8ffaba97ae870bddc60f3e287b8a866b4e92
alamin <alaminhosen44000@gmail.com>
2023-11-02 21:47:03 +0600

    j

--- js/main.js
+++ js/main.js
@@ -42,31 +42,44 @@
 // slider
 {
   let pSlider = document.querySelector(".product-slider-active");
-  let pSliderDotsParent = document.querySelector(".product-slider-dots");
-  let pSliderDots = document.querySelectorAll(".product-slider-dots .dot");
-  let pSliderElements = pSlider.children;
-  console.log(typeof pSliderElements);
-  pSliderDots.forEach((dot) => {
-    dot.addEventListener("click", function () {
-      //dots-update
-      pSliderDots.forEach((dotE) => {
-        dotE.classList.remove("active");
-      });
-      dot.classList.add("active");
+  if (pSlider !== null) {
+    let pSliderDotsParent = document.querySelector(".product-slider-dots");
+    let pSliderDots = document.querySelectorAll(".product-slider-dots .dot");
+    let pSliderElements = pSlider.children;
+    console.log(typeof pSliderElements);
+    pSliderDots.forEach((dot) => {
+      dot.addEventListener("click", function () {
+        //dots-update
+        pSliderDots.forEach((dotE) => {
+          dotE.classList.remove("active");
+        });
+        dot.classList.add("active");
 
-      // slider-update
-      Array.from(pSliderElements).forEach((eachElm) => {
-        eachElm.classList.remove("active");
+        // slider-update
+        Array.from(pSliderElements).forEach((eachElm) => {
+          eachElm.classList.remove("active");
+        });
+        let dotIndex = Array.prototype.indexOf.call(
+          pSliderDotsParent.children,
+          dot
+        );
+        let targetSlide = document.querySelector(
+          `.product-slider-active .slider-item:nth-child(${dotIndex + 1})`
+        );
+        targetSlide.classList.add("active");
       });
-      let dotIndex = Array.prototype.indexOf.call(
-        pSliderDotsParent.children,
-        dot
-      );
-      let targetSlide = document.querySelector(
-        `.product-slider-active .slider-item:nth-child(${dotIndex + 1})`
-      );
-      targetSlide.classList.add("active");
     });
-  });
-  console.log(pSliderElements);
+  }
+}
+
+//filter-dropdown-height
+{
+  let productCase = document.querySelector(".product__case");
+  let filterPanelDesktop = document.querySelector(
+    "#case-filter__panel__desktop"
+  );
+  filterPanelDesktop.style.setProperty(
+    "--height-data",
+    productCase.offsetHeight + "px"
+  );
 }
